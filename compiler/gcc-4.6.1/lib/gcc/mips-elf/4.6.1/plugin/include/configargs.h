a460ec7578e50b9ba01e42f0b62b89d6fff69244
lairjr <lairjr@outlook.com>
2017-04-18 11:08:30 -0300

    Arrange files to use compiler

---
+++ compiler/gcc-4.6.1/lib/gcc/mips-elf/4.6.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/sergio/temp/build_toolchain/source/gcc-4.6.1/configure --prefix=/usr/local/mips/gcc-4.6.1 --target=mips-elf --with-gnu-as --with-gnu-ld --enable-languages=c --with-newlib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "synci", "no-synci" } };
